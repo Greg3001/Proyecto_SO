335099da78efd4fe7ef1627b24a29b667d1ab79d
Greg3001 <111346504+Greg3001@users.noreply.github.com>
2023-10-11 13:14:10 +0200

    Add files via upload

---
+++ Proyecto_V1/Interfaz.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Proyecto_V1
+{
+    partial class Interfaz
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.LabelCont = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.Color.LightSalmon;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 22.2F, System.Drawing.FontStyle.Bold);
+            this.button1.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.button1.Location = new System.Drawing.Point(1002, 578);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(225, 80);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "SIGN IN";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.AutoSizeChanged += new System.EventHandler(this.Form1_Load);
+            this.button1.FontChanged += new System.EventHandler(this.Form1_Load);
+            this.button1.LocationChanged += new System.EventHandler(this.Form1_Load);
+            this.button1.TextChanged += new System.EventHandler(this.Form1_Load);
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            this.button1.Move += new System.EventHandler(this.Form1_Load);
+            this.button1.Resize += new System.EventHandler(this.Form1_Load);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.BackColor = System.Drawing.Color.Coral;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI Semibold", 22.2F, System.Drawing.FontStyle.Bold);
+            this.button2.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.button2.Location = new System.Drawing.Point(668, 578);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(225, 80);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "LOG IN";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.FontChanged += new System.EventHandler(this.Form1_Load);
+            this.button2.LocationChanged += new System.EventHandler(this.Form1_Load);
+            this.button2.TextChanged += new System.EventHandler(this.Form1_Load);
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.MouseEnter += new System.EventHandler(this.button2_MouseEnter);
+            this.button2.MouseLeave += new System.EventHandler(this.button2_MouseLeave);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Segoe UI Semibold", 16.2F, System.Drawing.FontStyle.Bold);
+            this.button3.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.button3.Location = new System.Drawing.Point(1645, 928);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(248, 54);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "DESCONECTAR";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.FontChanged += new System.EventHandler(this.Form1_Load);
+            this.button3.LocationChanged += new System.EventHandler(this.Form1_Load);
+            this.button3.TabStopChanged += new System.EventHandler(this.Form1_Load);
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.MouseEnter += new System.EventHandler(this.button3_MouseEnter);
+            this.button3.MouseLeave += new System.EventHandler(this.button3_MouseLeave);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button4.AutoSize = true;
+            this.button4.BackColor = System.Drawing.Color.Aquamarine;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Segoe UI Semibold", 22.2F, System.Drawing.FontStyle.Bold);
+            this.button4.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.button4.Location = new System.Drawing.Point(818, 791);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(236, 70);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "CONECTAR";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // LabelCont
+            // 
+            this.LabelCont.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.LabelCont.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LabelCont.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelCont.Location = new System.Drawing.Point(58, 27);
+            this.LabelCont.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LabelCont.Name = "LabelCont";
+            this.LabelCont.Size = new System.Drawing.Size(197, 113);
+            this.LabelCont.TabIndex = 14;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Segoe UI Semibold", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.button5.Location = new System.Drawing.Point(1648, 27);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(264, 69);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "HISTORIAL";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1924, 1055);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button6
+            // 
+            this.button6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button6.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Segoe UI Semibold", 22.2F, System.Drawing.FontStyle.Bold);
+            this.button6.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.button6.Location = new System.Drawing.Point(818, 344);
+            this.button6.Margin = new System.Windows.Forms.Padding(2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(274, 63);
+            this.button6.TabIndex = 16;
+            this.button6.Text = "MINIGAME";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Interfaz
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1924, 1055);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.LabelCont);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Interfaz";
+            this.Text = "Interfaz";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label LabelCont;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Proyecto_V1/Interfaz.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Proyecto_V1
+{
+    public partial class Interfaz : Form
+    {
+        Socket server;
+        Thread atender;
+
+        delegate void DelegadoParaPonerTexto(string texto);
+
+        List<Form2> formularios = new List<Form2>();
+        List<Form3> formularios3 = new List<Form3>();
+        public Interfaz()
+        {
+            InitializeComponent();
+        }
+        private void AtenderServidor()
+        {
+            while (true)
+            {
+                //Recibimos mensaje del servidor
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                string[] trozos = Encoding.ASCII.GetString(msg2).Split('/');
+                int codigo = Convert.ToInt32(trozos[0]);
+                string mensaje;
+
+                int nform;
+                switch (codigo)
+                {
+                    case 1:  // SIGN IN
+
+                        nform = Convert.ToInt32(trozos[1]);
+                        mensaje = trozos[2].Split('\0')[0];
+                        formularios[nform].TomaRespuesta1(mensaje);
+
+                        break;
+                    case 2:      // LOG IN
+
+                        nform = Convert.ToInt32(trozos[1]);
+                        mensaje = trozos[2].Split('\0')[0];
+                        formularios3[nform].TomaRespuesta2(mensaje);
+
+                        break;
+
+                    case 3:     //Recibimos notificacion
+
+
+                        mensaje = trozos[1].Split('\0')[0];
+
+                        //Haz tu lo que no me dejas hacer a mi
+                        LabelCont.Invoke(new Action(() =>
+                        {
+                            LabelCont.Text = mensaje;
+                        }));
+
+                        break;
+                }
+            }
+        }
+
+        //LOAD CON UNA IMAGEN DE FONDO (SE PODRIA HACER A PANTALLA COMPLETA)
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Set the form's border style to FixedSingle.
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+            //this.Size = new Size(1280, 720);
+            this.WindowState = FormWindowState.Maximized;
+            this.MaximizeBox = true;
+           
+            // Create an Image object.
+            Image image = Image.FromFile("FONDO.png");
+
+            // Display the image.
+            pictureBox1.Image = image;
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+        }
+
+        //BOTON1
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ThreadStart ts = delegate { PonerEnMarchaFormulario(); };
+            Thread T = new Thread(ts);
+            T.Start();
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            button1.Size = new Size((int)(button1.Width * 1.2f), (int)(button1.Height * 1.2f));
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.Size = new Size((int)(button1.Width / 1.2f), (int)(button1.Height / 1.2f));
+        }
+
+
+        //BOTON2
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ThreadStart ts = delegate { PonerEnMarchaFormulario2(); };
+            Thread T = new Thread(ts);
+            T.Start();
+        }
+        private void button2_MouseEnter(object sender, EventArgs e)
+        {
+            button2.Size = new Size((int)(button1.Width * 1.2f), (int)(button1.Height * 1.2f));
+        }
+
+        private void button2_MouseLeave(object sender, EventArgs e)
+        {
+            button2.Size = new Size((int)(button1.Width / 1.2f), (int)(button1.Height / 1.2f));
+        }
+
+
+        //DESCONECTAR
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Mensaje de desconexión
+            string mensaje = "0/";
+
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+
+            // Nos desconectamos
+            atender.Abort();
+            this.BackColor = Color.Gray;
+            server.Shutdown(SocketShutdown.Both);
+            server.Close();
+        }
+        private void button3_MouseEnter(object sender, EventArgs e)
+        {
+            button3.Size = new Size((int)(button1.Width * 1.2f), (int)(button1.Height * 1.2f));
+        }
+
+        private void button3_MouseLeave(object sender, EventArgs e)
+        {
+            button3.Size = new Size((int)(button1.Width / 1.2f), (int)(button1.Height / 1.2f));
+        }
+
+        //CONECTAR CON EL SERVIDOR
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Creamos un IPEndPoint con el ip del servidor y puerto del servidor 
+            //al que deseamos conectarnos
+            IPAddress direc = IPAddress.Parse("192.168.56.102");
+            IPEndPoint ipep = new IPEndPoint(direc, 9070);
+
+
+            //Creamos el socket 
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Connect(ipep);//Intentamos conectar el socket
+                this.BackColor = Color.Green;
+                MessageBox.Show("Conectado");
+                //pongo en marcha el thread que atenderá los mensajes del servidor
+                ThreadStart ts = delegate { AtenderServidor(); };
+                atender = new Thread(ts);
+                atender.Start();
+
+            }
+            catch (SocketException ex)
+            {
+                //Si hay excepcion imprimimos error y salimos del programa con return 
+                MessageBox.Show("No he podido conectar con el servidor");
+                return;
+            }
+        }
+        private void button4_MouseEnter(object sender, EventArgs e)
+        {
+            button2.Size = new Size((int)(button1.Width * 1.2f), (int)(button1.Height * 1.2f));
+        }
+
+        private void button4_MouseLeave(object sender, EventArgs e)
+        {
+            button2.Size = new Size((int)(button1.Width / 1.2f), (int)(button1.Height / 1.2f));
+        }
+
+        private void PonerEnMarchaFormulario()
+        {
+            int cont = formularios.Count;
+            Form2 f = new Form2(cont, server);
+            formularios.Add(f);
+            f.ShowDialog();
+        }
+        private void PonerEnMarchaFormulario2()
+        {
+            int cont = formularios3.Count;
+            Form3 f3 = new Form3(cont, server);
+            formularios3.Add(f3);
+            f3.ShowDialog();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form historial = new Historial();
+            historial.Show();
+
+        }
+        private void PonerEnMarchaMinigame()
+        {
+            int cont = formularios3.Count;
+            Form minigame = new Minigame(cont, server);
+
+            minigame.ShowDialog();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ThreadStart ts = delegate { PonerEnMarchaMinigame(); };
+            Thread T = new Thread(ts);
+            T.Start();
+        }
+    }
+}
