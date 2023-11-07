541567ac3b7e70eafbf4071552fb26635c1fc862
angela667 <angelaciutatcamps@gmail.com>
2023-11-07 20:27:23 +0100

    v1

---
+++ Proyecto_V1/Minigame.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Proyecto_V1
+{
+    public partial class Minigame : Form
+    {
+        int nForm;
+        Socket server;
+        public Minigame(int nForm, Socket server)
+        {
+            InitializeComponent();
+            this.nForm = nForm;
+            this.server = server;
+        }
+        public void TomaRespuesta1(string mensaje)
+        {
+            MessageBox.Show(mensaje);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string mensaje = "3/"  +  textBox1.Text;
+            // Enviamos al servidor el nombre tecleado
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string mensaje = "4/"  + textBox1.Text;
+            // Enviamos al servidor el nombre tecleado
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string mensaje = "5/"  + textBox1.Text;
+            // Enviamos al servidor el nombre tecleado
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# ProyectoS0
+Proyecto del grupo Cuttlefish de Sistemas Operativos (2023-24-Q1)
+DRAGON BALL
